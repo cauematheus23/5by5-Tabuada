9ca7445a92c575035962e8c76c993e4ff907de3c
cauematheus23 <cauematheus_23@hotmail.com>
2024-04-18 15:46:33 -0300

    Add project files.

---
+++ Tabuada/Program.cs
@@ -0,0 +1,15 @@
+﻿int x, i;
+Console.WriteLine("Digite o valor de x para ver sua tabuada: ");
+x = int.Parse(Console.ReadLine());
+if (x <= 0)
+{
+    Console.WriteLine("Digite um valor positivo");
+}
+else
+{
+    for (i = 0; i <= 10; i++)
+    {
+        Console.WriteLine($"{x}x{i} = {x * i}");
+
+    }
+}
